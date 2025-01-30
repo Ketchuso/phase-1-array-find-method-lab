0f4b1c129374c1e4403c65c8d5ed76fe043f2b1a
Jay Davis <jdlikesbsbl@gmail.com>
2025-01-30 11:29:24 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array){
+    const winningResult = array.find(yearFinder)
+    if (winningResult && winningResult.result === "W"){
+        return winningResult.year;
+    }
+}
+
+function yearFinder(element){
+    return element.result === "W";
+}
